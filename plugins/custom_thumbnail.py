84d809823ad6b6792b0a23b4181322634eabee64
gvijay-tech <60471164+gvijay-tech@users.noreply.github.com>
2020-05-02 20:23:07 +0530

    Update custom_thumbnail.py

--- plugins/custom_thumbnail.py
+++ plugins/custom_thumbnail.py
@@ -1,141 +1,66 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# (c) Shrimadhav U K
+"""ThumbNail utilities, © @AnyDLBot"""
 
-# the logging things
-import logging
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logger = logging.getLogger(__name__)
 
-import numpy
 import os
+
+from tobrot import DOWNLOAD_LOCATION
+
+from hachoir.metadata import extractMetadata
+from hachoir.parser import createParser
 from PIL import Image
-import time
-
-# the secret configuration specific things
-if bool(os.environ.get("WEBHOOK", False)):
-    from sample_config import Config
-else:
-    from config import Config
-
-# the Strings used for this "thing"
-from translation import Translation
-
-import pyrogram
-logging.getLogger("pyrogram").setLevel(logging.WARNING)
-
-from helper_funcs.chat_base import TRChatBase
 
 
-@pyrogram.Client.on_message(pyrogram.Filters.command(["generatecustomthumbnail"]))
-async def generate_custom_thumbnail(bot, update):
-    if update.from_user.id not in Config.AUTH_USERS:
-        await bot.delete_messages(
-            chat_id=update.chat.id,
-            message_ids=update.message_id,
-            revoke=True
-        )
-        return
-    TRChatBase(update.from_user.id, update.text, "generatecustomthumbnail")
-    if update.reply_to_message is not None:
-        reply_message = update.reply_to_message
-        if reply_message.media_group_id is not None:
-            download_location = Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id) + "/" + str(reply_message.media_group_id) + "/"
-            save_final_image = download_location + str(round(time.time())) + ".jpg"
-            list_im = os.listdir(download_location)
-            if len(list_im) == 2:
-                imgs = [ Image.open(download_location + i) for i in list_im ]
-                inm_aesph = sorted([(numpy.sum(i.size), i.size) for i in imgs])
-                min_shape = inm_aesph[1][1]
-                imgs_comb = numpy.hstack(numpy.asarray(i.resize(min_shape)) for i in imgs)
-                imgs_comb = Image.fromarray(imgs_comb)
-                # combine: https://stackoverflow.com/a/30228789/4723940
-                imgs_comb.save(save_final_image)
-                # send
-                await bot.send_photo(
-                    chat_id=update.chat.id,
-                    photo=save_final_image,
-                    caption=Translation.CUSTOM_CAPTION_UL_FILE,
-                    reply_to_message_id=update.message_id
-                )
-            else:
-                await bot.send_message(
-                    chat_id=update.chat.id,
-                    text=Translation.ERR_ONLY_TWO_MEDIA_IN_ALBUM,
-                    reply_to_message_id=update.message_id
-                )
-            try:
-                [os.remove(download_location + i) for i in list_im ]
-                os.remove(download_location)
-            except:
-                pass
-        else:
-            await bot.send_message(
-                chat_id=update.chat.id,
-                text=Translation.REPLY_TO_MEDIA_ALBUM_TO_GEN_THUMB,
-                reply_to_message_id=update.message_id
-            )
-    else:
-        await bot.send_message(
-            chat_id=update.chat.id,
-            text=Translation.REPLY_TO_MEDIA_ALBUM_TO_GEN_THUMB,
-            reply_to_message_id=update.message_id
-        )
-
-
-@pyrogram.Client.on_message(pyrogram.Filters.photo)
-async def save_photo(bot, update):
-    if update.from_user.id not in Config.AUTH_USERS:
-        await bot.delete_messages(
-            chat_id=update.chat.id,
-            message_ids=update.message_id,
-            revoke=True
-        )
-        return
-    TRChatBase(update.from_user.id, update.text, "save_photo")
-    if update.media_group_id is not None:
-        # album is sent
-        download_location = Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id) + "/" + str(update.media_group_id) + "/"
-        # create download directory, if not exist
-        if not os.path.isdir(download_location):
-            os.makedirs(download_location)
-        await bot.download_media(
-            message=update,
-            file_name=download_location
-        )
-    else:
-        # received single photo
-        download_location = Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id) + ".jpg"
-        await bot.download_media(
-            message=update,
-            file_name=download_location
-        )
-        await bot.send_message(
-            chat_id=update.chat.id,
-            text=Translation.SAVED_CUSTOM_THUMB_NAIL,
-            reply_to_message_id=update.message_id
-        )
-
-
-@pyrogram.Client.on_message(pyrogram.Filters.command(["deletethumbnail"]))
-async def delete_thumbnail(bot, update):
-    if update.from_user.id not in Config.AUTH_USERS:
-        await bot.delete_messages(
-            chat_id=update.chat.id,
-            message_ids=update.message_id,
-            revoke=True
-        )
-        return
-    TRChatBase(update.from_user.id, update.text, "deletethumbnail")
-    download_location = Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id)
-    try:
-        os.remove(download_location + ".jpg")
-        # os.remove(download_location + ".json")
-    except:
-        pass
-    await bot.send_message(
-        chat_id=update.chat.id,
-        text=Translation.DEL_ETED_CUSTOM_THUMB_NAIL,
-        reply_to_message_id=update.message_id
+async def save_thumb_nail(client, message):
+    thumbnail_location = os.path.join(
+        DOWNLOAD_LOCATION,
+        "thumbnails"
     )
+    thumb_image_path = os.path.join(
+        thumbnail_location,
+        str(message.from_user.id) + ".jpg"
+    )
+    ismgs = await message.reply_text("processing ...")
+    if message.reply_to_message is not None:
+        if not os.path.isdir(thumbnail_location):
+            os.makedirs(thumbnail_location)
+        download_location = thumbnail_location + "/"
+        downloaded_file_name = await client.download_media(
+            message=message.reply_to_message,
+            file_name=download_location
+        )
+        # https://stackoverflow.com/a/21669827/4723940
+        Image.open(downloaded_file_name).convert("RGB").save(downloaded_file_name)
+        metadata = extractMetadata(createParser(downloaded_file_name))
+        height = 0
+        if metadata.has("height"):
+            height = metadata.get("height")
+        # resize image
+        # ref: https://t.me/PyrogramChat/44663
+        img = Image.open(downloaded_file_name)
+        # https://stackoverflow.com/a/37631799/4723940
+        # img.thumbnail((320, 320))
+        img.resize((320, height))
+        img.save(thumb_image_path, "JPEG")
+        # https://pillow.readthedocs.io/en/3.1.x/reference/Image.html#create-thumbnails
+        os.remove(downloaded_file_name)
+        await ismgs.edit(
+            "Custom video / file thumbnail saved. " + \
+            "This image will be used in the upload, till /clearthumbnail."
+        )
+    else:
+        await message.edit("Reply to a photo to save custom thumbnail")
+
+
+async def clear_thumb_nail(client, message):
+    thumbnail_location = os.path.join(
+        DOWNLOAD_LOCATION,
+        "thumbnails"
+    )
+    thumb_image_path = os.path.join(
+        thumbnail_location,
+        str(message.from_user.id) + ".jpg"
+    )
+    ismgs = await message.reply_text("processing ...")
+    if os.path.exists(thumb_image_path):
+        os.remove(thumb_image_path)
+    await ismgs.edit("✅ Custom thumbnail cleared succesfully.")
